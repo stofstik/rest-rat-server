c3d3975b16e9dad87c1541ae3b3dfe40728becea
stofstik <verstegen.daan@gmail.com>
2015-12-16 20:09:07 +0100

    work work

--- server.js
+++ server.js
@@ -23,23 +23,17 @@ app.use(morgan(':date[clf] :remote-addr :method :url :res[content-length] - :res
 
 app.post('/handshake/:id/:version/:package/:ssid/:isAudioStarted/:isLocationStarted/:ftpServerStarted/:location',
 		function(req, res) {
-			// morgan.log(req.params.id);
-			// morgan.log(req.params.version);
-			// morgan.log(req.params.package);
-			// morgan.log(req.params.ssid);
-			// morgan.log(req.params.isAudioStarted);
-			// morgan.log(req.params.isLocationStarted);
-
-			// TODO html dashboard where we can edit actions per connection
-			//
-			var actions = {
-				startAudio    : false,
-				startLocation : false,
-				startFTPServer: false
-			};
 
 			connectionManager.add(req.params.id, req.params.isLocationStarted,
-					req.params.isAudioStarted, req.params.ftpServerStarted);
+					req.params.isAudioStarted, req.params.ftpServerStarted,
+					req.params.location);
+
+			var connection = connectionManager.getOneById(req.params.id);
+			var actions = {
+				startAudio    : connection.audio,
+				startLocation : connection.locationStarted,
+				startFTPServer: connection.ftpServ
+			};
 
 			res.send(actions);
 		});
@@ -58,28 +52,36 @@ app.get('/dashboard', function(req, res) {
 		content += " " + connections[i].age;
 		content += "</a>";
 		// link to start stop location
-		content += " " + "location: ";
-		content += "<a href=\""
+		content += " " + "location started: ";
+		content += "<a href=\"";
 		content += "/toggleLocation/";
 		content += connections[i].id;
 		content += "\">";
-		content += " " + connections[i].location;
+		content += " " + connections[i].locationStarted;
 		content += "</a>";
 		// link to start stop audio
-		content += " " + "audio: ";
-		content += "<a href=\""
+		content += " " + "audio started: ";
+		content += "<a href=\"";
 		content += "/toggleAudio/";
 		content += connections[i].id;
 		content += "\">";
 		content += " " + connections[i].audio;
 		content += "</a>";
 		// link to start stop ftp server
-		content += " " + "ftpServer: ";
-		content += "<a href=\""
+		content += " " + "ftpServer started: ";
+		content += "<a href=\"";
 		content += "/toggleFtpServer/";
 		content += connections[i].id;
 		content += "\">";
 		content += " " + connections[i].ftpServ;
+		content += "</a>";
+		content += " " + "last location: ";
+		content += "<a target=\"_blank\" href=\"";
+		content += "http://maps.google.com/maps?q=loc:";
+		content += connections[i].location;
+		content += "\">";
+		content += " " + connections[i].location;
+		content += "</a>";
 	}
 	var foot = "\r\n</body> </html>";
 	var page = head + content + foot;
@@ -97,64 +99,66 @@ app.get('/detailView/:id', function (req, res) {
 
 app.get('/toggleFtpServer/:id', function (req, res) {
 	var connection = connectionManager.getOneById(req.params.id);
-	if(connection.ftpServ === false){
-		connection.ftpServ = true;
-	} else {
+	if(connection.ftpServ === true || connection.ftpServ === "true"){
 		connection.ftpServ = false;
+	} else {
+		connection.ftpServ = true;
 	}
 	res.redirect('/dashboard');
 });
 
 app.get('/toggleLocation/:id', function (req, res) {
 	var connection = connectionManager.getOneById(req.params.id);
-	if(connection.location === false){
-		connection.location = true;
+	if(connection.locationStarted === true || connection.locationStarted === "true"){
+		connection.locationStarted = false;
 	} else {
-		connection.location = false;
+		connection.locationStarted = true;
 	}
 	res.redirect('/dashboard');
 });
 
 app.get('/toggleAudio/:id', function (req, res) {
 	var connection = connectionManager.getOneById(req.params.id);
-	if(connection.audio === false){
-		connection.audio = true;
-	} else {
+	if(connection.audio === true || connection.audio === "true"){
 		connection.audio = false;
+	} else {
+		connection.audio = true;
 	}
 	res.redirect('/dashboard');
 });
 
 var connectionManager = {
 	connections: [],
-	add: function(id, location, audio, ftpServ){
-		this.connections.push( {
-			id: id,
-			age: Date.now(),
-			location: location,
-			audio: audio,
-			ftpServ: ftpServ
-		});
+	add: function(id, locationStarted, audio, ftpServ, location){
+		if(this.getOneById(id) === undefined){
+			this.connections.push( {
+				id: id,
+				age: Date.now(),
+				locationStarted: locationStarted,
+				audio: audio,
+				ftpServ: ftpServ,
+				location: location
+			});
+		}
 		this.clean();
 	},
 	clean: function(){
 		var timeout = 60000;
-		for(var conn in this.connections){
-			if(this.connections[conn].age < (Date.now() - timeout)){
-				var index = this.connections.indexOf(this.connections[conn]);
+		for(var i = 0; i < this.connections.length; i++){
+			if(this.connections[i].age < (Date.now() - timeout)){
+				var index = this.connections.indexOf(this.connections[i]);
 				this.connections.splice(index, 1);
 			}
 		}
 	},
 	getOneById: function(id){
-		for(var conn in this.connections){
-			console.log("searching: " + this.connections[conn]);
-			if(this.connections[conn].id === id) {
-				return this.connections[conn];
+		for(var i = 0; i < this.connections.length; i++){
+			if(this.connections[i].id === id) {
+				return this.connections[i];
 			}
 		}
 	}
-}
+};
 
 // start the server
 var server = app.listen(PORT_NUMBER, function () {
